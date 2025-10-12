a5fba1eaea45a923a8b1fb27dab1c9d1d948b28f
pepe_el_pollo <al350553@edu.uaa.mx>
2025-10-12 14:55:54 -0600

    Barra de navegacion inferior

--- app/src/main/java/com/example/proyecto_final/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_final/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.proyecto_final
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,9 +17,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
-        var lol = "Prueba"
+        var bottomnav : BottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val menu = bottomnav.menu
+        menu.findItem(R.id.bottom_home).icon = ContextCompat.getDrawable(this,R.drawable.baseline_home_filled_24)
+        bottomnav.itemIconTintList = null
+        val orange = ColorStateList.valueOf(Color.argb(255, 255, 204, 128))
+        bottomnav.itemActiveIndicatorColor = orange
     }
 }
